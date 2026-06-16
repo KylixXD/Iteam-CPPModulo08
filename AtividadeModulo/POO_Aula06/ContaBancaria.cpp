362985a267ee312073957154a808aa9cf0a690c5
Rafael Nobrega de Lima <61426596+KylixXD@users.noreply.github.com>
2026-06-16 09:44:13 -0400

    Add files via upload

---
+++ AtividadeModulo/POO_Aula06/ContaBancaria.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ContaBancaria {
+    private:
+        string titular;
+        double saldo;
+
+    public:
+        ContaBancaria(string titular, double saldoInicial) {
+            this->titular = titular;
+            this->saldo = saldoInicial;
+        }
+
+        void depositar(double valor) {
+            if (valor > 0) {
+                this->saldo += valor;
+                cout << "Depósito de R$ " << valor << " realizado com sucesso!" << endl;
+            } else {
+                cout << "Valor de depósito inválido!" << endl;
+            }
+        }
+
+        void sacar(double valor) {
+            if (valor > 0 && valor <= this->saldo) {
+                this->saldo -= valor;
+                cout << "Saque de R$ " << valor << " realizado com sucesso!" << endl;
+            } else if (valor > this->saldo) {
+                cout << "Saldo insuficiente para realizar o saque de R$ " << valor << "!" << endl;
+            } else {
+                cout << "Valor de saque inválido!" << endl;
+            }
+        }
+
+        void mostrarSaldo() {
+            cout << "-----------------------------------" << endl;
+            cout << "Titular da Conta: " << this->titular << endl;
+            cout << "Saldo Atual: R$ " << this->saldo << endl;
+            cout << "-----------------------------------" << endl;
+        }
+};
+
+int main() {
+    ContaBancaria conta1("Rafael", 1000.0);
+    
+
+    conta1.mostrarSaldo();
+    conta1.depositar(500.0);
+
+    conta1.sacar(300.0);
+    conta1.mostrarSaldo();
+
+    return 0;
+}
